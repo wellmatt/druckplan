873e5cc8bfaa25b4de5d25880cf400659b21860e
Christian Schröer <cschroeer@ipactor.de>
2016-11-04 09:35:37 +0100

    stuff

---
+++ libs/modules/accounting/invoicein.class.php
@@ -0,0 +1,221 @@
+<?php
+/**
+ *  Copyright (c) 2016 Klein Druck + Medien GmbH - All Rights Reserved
+ *  * Unauthorized modification or copying of this file, via any medium is strictly prohibited
+ *  * Proprietary and confidential
+ *  * Written by Christian Schroeer <cschroeer@ipactor.de>, 2016
+ *
+ */
+require_once 'libs/basic/model.php';
+
+class InvoiceIn extends Model{
+    public $_table = 'invoiceins';
+
+    public $number;
+    public $colinv = 0;
+    public $netvalue = 0.0;
+    public $grossvalue = 0.0;
+    public $cost = 0.0;
+    public $crtdate = 0;
+    public $duedate = 0;
+    public $payeddate = 0;
+    public $status = 1;
+
+    const STATE_DELETED = 0;
+    const STATE_OPEN = 1;
+    const STATE_PAYED = 2;
+    const STATE_STORNO = 3;
+
+
+    protected function bootClasses()
+    {
+        $this->colinv = new CollectiveInvoice($this->colinv);
+    }
+
+    /**
+     * @param $number
+     * @param CollectiveInvoice $colinv
+     * @param PaymentTerms $payterm
+     * @return InvoiceIn
+     */
+    public static function generate($number, $colinv, $payterm)
+    {
+        $nettodays = $payterm->getNettodays();
+        $now = time();
+        $aday = 86400;
+        $netvalue = 0.0;
+        $grossvalue = 0.0;
+        $cost = 0.0;
+
+        $duedate = ($now + ($nettodays * $aday));
+
+        $tax = [];
+        $positions = Orderposition::getAllOrderposition($colinv->getId());
+        foreach ($positions as $position) {
+            $art = new Article($position->getObjectid());
+            if ($art->getOrderid() > 0){
+                $netto = $position->getPrice();
+                $postax = $position->getTax();
+                $poscost = $position->getCost();
+                $tax[$postax][] = [$netto,$poscost];
+            } else {
+                $netto = $position->getPrice() * $position->getAmount();
+                $postax = $position->getTax();
+                $poscost = $position->getCost() * $position->getAmount();
+                $tax[$postax][] = [$netto,$poscost];
+            }
+        }
+
+        foreach ($tax as $mwst => $items) {
+            foreach ($items as $item) {
+                $netvalue += $item[0];
+                $grossvalue += $item[0] * (1 + $mwst/100);
+                $cost += $item[1];
+            }
+        }
+
+        $array = [
+            'number' => $number,
+            'colinv' => $colinv->getId(),
+            'netvalue' => $netvalue,
+            'grossvalue' => $grossvalue,
+            'cost' => $cost,
+            'crtdate' => time(),
+            'duedate' => $duedate,
+        ];
+
+        $invout = new InvoiceIn(0,$array);
+        $invout->save();
+        return $invout;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * @param string $number
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+    }
+
+    /**
+     * @return CollectiveInvoice
+     */
+    public function getColinv()
+    {
+        return $this->colinv;
+    }
+
+    /**
+     * @param CollectiveInvoice $colinv
+     */
+    public function setColinv($colinv)
+    {
+        $this->colinv = $colinv;
+    }
+
+    /**
+     * @return float
+     */
+    public function getNetvalue()
+    {
+        return $this->netvalue;
+    }
+
+    /**
+     * @param float $netvalue
+     */
+    public function setNetvalue($netvalue)
+    {
+        $this->netvalue = $netvalue;
+    }
+
+    /**
+     * @return float
+     */
+    public function getGrossvalue()
+    {
+        return $this->grossvalue;
+    }
+
+    /**
+     * @param float $grossvalue
+     */
+    public function setGrossvalue($grossvalue)
+    {
+        $this->grossvalue = $grossvalue;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDuedate()
+    {
+        return $this->duedate;
+    }
+
+    /**
+     * @param int $duedate
+     */
+    public function setDuedate($duedate)
+    {
+        $this->duedate = $duedate;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPayeddate()
+    {
+        return $this->payeddate;
+    }
+
+    /**
+     * @param int $payeddate
+     */
+    public function setPayeddate($payeddate)
+    {
+        $this->payeddate = $payeddate;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param int $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCrtdate()
+    {
+        return $this->crtdate;
+    }
+
+    /**
+     * @param int $crtdate
+     */
+    public function setCrtdate($crtdate)
+    {
+        $this->crtdate = $crtdate;
+    }
+
+
+}
---
+++ libs/modules/accounting/invoicein.overview.php
@@ -0,0 +1,191 @@
+<?php
+/**
+ *  Copyright (c) 2016 Klein Druck + Medien GmbH - All Rights Reserved
+ *  * Unauthorized modification or copying of this file, via any medium is strictly prohibited
+ *  * Proprietary and confidential
+ *  * Written by Christian Schroeer <cschroeer@ipactor.de>, 2016
+ *
+ */
+
+
+?>
+<link rel="stylesheet" type="text/css" href="css/jquery.dataTables.css">
+<link rel="stylesheet" type="text/css" href="css/dataTables.bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/dataTables.tableTools.css">
+
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h3 class="panel-title">
+            Rechnungseingang
+            <span class="pull-right">
+                <button class="btn btn-xs btn-success" onclick="document.location. href='index.php?page=libs/modules/accounting/invoicein.edit.php&exec=new';">
+                     <span class="glyphicons glyphicons-plus"></span>
+                     <?=$_LANG->get('Rechnung hinzuf&uuml;gen')?>
+                 </button>
+                <button class="btn btn-xs btn-success" type="button" onclick="invoutexport();">Export</button>
+            </span>
+        </h3>
+    </div>
+    <div class="panel-body">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Filter</h3>
+            </div>
+            <div class="panel-body">
+                <div class="form-group">
+                    <label for="" class="col-sm-2 control-label">Datum (erstellt)</label>
+                    <div class="col-sm-4">
+                        <input name="ajax_date_min" id="ajax_date_min" type="hidden"/>
+                        <input name="date_min" id="date_min" class="form-control" onfocus="markfield(this,0)" onblur="markfield(this,1)">
+                    </div>
+                    <label for="" class="col-sm-2 control-label">Bis:</label>
+                    <div class="col-sm-4">
+                        <input name="ajax_date_max" id="ajax_date_max" type="hidden"/>
+                        <input name="date_max" id="date_max" class="form-control" onfocus="markfield(this,0)" onblur="markfield(this,1)">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="" class="col-sm-3 control-label">Suche:</label>
+                    <div class="col-sm-9">
+                        <input type="text" id="search" class="form-control" placeholder="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="table-responsive">
+            <table class="table table-hover" id="invintable">
+                <thead>
+                <tr>
+                    <th>RE-Datum</th>
+                    <th>Re.-Nr.</th>
+                    <th>Lieferant</th>
+                    <th>Grund der Ausgabe</th>
+                    <th>Netto</th>
+                    <th>Brutto</th>
+                    <th>Erstellt</th>
+                    <th>Fällig</th>
+                    <th>Bezahlt</th>
+                </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+</div>
+
+<!-- DataTables -->
+<script type="text/javascript" charset="utf8" src="jscripts/datatable/jquery.dataTables.min.js"></script>
+<script type="text/javascript" charset="utf8" src="jscripts/datatable/numeric-comma.js"></script>
+<script type="text/javascript" charset="utf8" src="jscripts/datatable/dataTables.bootstrap.js"></script>
+<script type="text/javascript" charset="utf8" src="jscripts/datatable/dataTables.tableTools.js"></script>
+<script type="text/javascript">
+    function invoutexport(){
+        var datemin = parseInt($('#ajax_date_min').val());
+        var datemax = parseInt($('#ajax_date_max').val());
+        window.open('libs/modules/accounting/invoicein.export.php?datemax='+datemax+'&datemin='+datemin);
+    }
+
+    $(document).ready(function() {
+
+        $.datepicker.setDefaults($.datepicker.regional['<?=$_LANG->getCode()?>']);
+        $('#date_min').datepicker(
+            {
+                showOtherMonths: true,
+                selectOtherMonths: true,
+                dateFormat: 'dd.mm.yy',
+                onSelect: function(selectedDate) {
+                    $('#ajax_date_min').val(moment($('#date_min').val(), "DD-MM-YYYY").unix());
+                    $('#invintable').dataTable().fnDraw();
+                }
+            }
+        );
+
+        $('#date_max').datepicker(
+            {
+                showOtherMonths: true,
+                selectOtherMonths: true,
+                dateFormat: 'dd.mm.yy',
+                onSelect: function(selectedDate) {
+                    $('#ajax_date_max').val(moment($('#date_max').val(), "DD-MM-YYYY").unix()+86340);
+                    $('#invinstable').dataTable().fnDraw();
+                }
+            }
+        );
+
+        var invouttable = $('#invinstable').DataTable( {
+            "processing": true,
+            "bServerSide": true,
+            "sAjaxSource": "libs/modules/accounting/invoicein.dt.ajax.php",
+            "paging": true,
+            "stateSave": <?php if($perf->getDt_state_save()) {echo "true";}else{echo "false";};?>,
+            "pageLength": <?php echo $perf->getDt_show_default();?>,
+            "dom": 'T<"clear">lrtip',
+            "tableTools": {
+                "sSwfPath": "jscripts/datatable/copy_csv_xls_pdf.swf",
+                "aButtons": [
+                    "copy",
+                    "csv",
+                    "xls",
+                    {
+                        "sExtends": "pdf",
+                        "sPdfOrientation": "landscape",
+                        "sPdfMessage": "Contilas - Businesscontacts"
+                    },
+                    "print"
+                ]
+            },
+            "lengthMenu": [ [10, 25, 50, 100, 250, -1], [10, 25, 50, 100, 250, "Alle"] ],
+            "columns": [
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+            ],
+            "language":
+            {
+                "emptyTable":     "Keine Daten vorhanden",
+                "info":           "Zeige _START_ bis _END_ von _TOTAL_ Eintr&auml;gen",
+                "infoEmpty": 	  "Keine Seiten vorhanden",
+                "infoFiltered":   "(gefiltert von _MAX_ gesamten Eintr&auml;gen)",
+                "infoPostFix":    "",
+                "thousands":      ".",
+                "lengthMenu":     "Zeige _MENU_ Eintr&auml;ge",
+                "loadingRecords": "Lade...",
+                "processing":     "Verarbeite...",
+                "search":         "Suche:",
+                "zeroRecords":    "Keine passenden Eintr&auml;ge gefunden",
+                "paginate": {
+                    "first":      "Erste",
+                    "last":       "Letzte",
+                    "next":       "N&auml;chste",
+                    "previous":   "Vorherige"
+                },
+                "aria": {
+                    "sortAscending":  ": aktivieren um aufsteigend zu sortieren",
+                    "sortDescending": ": aktivieren um absteigend zu sortieren"
+                }
+            },
+            "fnServerParams": function ( aoData ) {
+                var iMin = document.getElementById('ajax_date_min').value;
+                var iMax = document.getElementById('ajax_date_max').value;
+                aoData.push( { "name": "start", "value": iMin, } );
+                aoData.push( { "name": "end", "value": iMax, } );
+            }
+        });
+
+        $('#search').keyup(function(){
+            invouttable.search( $(this).val() ).draw();
+        });
+
+        $("#invinstable tbody td").live('click',function(){
+            var aPos = $('#invinstable').dataTable().fnGetPosition(this);
+            var aData = $('#invinstable').dataTable().fnGetData(aPos[0]);
+            document.location='index.php?page=libs/modules/accounting/invoicein.edit.php&exec=edit&id='+aData[0];
+        });
+
+    } );
+</script>
