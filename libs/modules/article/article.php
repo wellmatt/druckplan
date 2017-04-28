90d1f47b8c4a372200803fc48cbb4c3c7b2bf480
Alexander Scherer <schealex@users.noreply.github.com>
2017-04-28 06:21:58 +0200

    fixes Order to Article saving

--- libs/modules/article/article.php
+++ libs/modules/article/article.php
@@ -41,7 +41,7 @@ switch ($_REQUEST["exec"]) {
 	        $article->setOrderid((int)$_REQUEST["orderid"]);
 	        $tmp_order = new Order((int)$_REQUEST["orderid"]);
 	        $article->setTitle($tmp_order->getTitle());
-			$article->setRevenueaccount(new RevenueAccount());
+			$article->setRevenueaccount(new RevenueaccountCategory());
 	        $firstcalc = new Calculation();
 	        if ($tmp_order->getId()>0)
 	        {
